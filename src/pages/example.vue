2f8bd85f15d0dbfd11428e61beb91685e038bd8b
sopheak <sopheak@acrossbyte.com>
2025-08-30 11:35:02 +0700

    feat: add example page with VPie chart component v3.9.3

---
+++ src/pages/example.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <VPie
+      title="Basic pie"
+      animation
+      legend
+      :palette="['#048BA8', '#99C24D', '#F18F01']"
+      tooltip
+      reveal
+      :items="items"
+    />
+    1
+  </div>
+</template>
+
+<script setup lang="ts">
+import { VPie } from 'vuetify/labs/VPie';
+
+  const items = [
+    { key: 1, title: "Yes", value: 45 },
+    { key: 2, title: "No", value: 40 },
+    { key: 3, title: "Maybe", value: 15 },
+  ]
+</script>
+
+<style scoped>
+
+</style>
